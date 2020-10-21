a1af9657e8064f8cc76cfe484d9ef0a479744c87
Jaedyn Ramirez <jaedynramirez@Jaedyns-MacBook-Pro.local>
2020-10-21 15:04:41 -0500

    properly deploys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
   firebaseConfig: {
     apiKey: "AIzaSyDma9r5Tt23RfoFgj7EiTZLEKHHhCByhMQ",
-    authDomain: "firestore-9ee8b.firebaseapp.com",
-    databaseURL: "https://firestore-9ee8b.firebaseio.com",
-    projectId: "firestore-9ee8b",
-    storageBucket: "firestore-9ee8b.appspot.com",
-    messagingSenderId: "838723952514",
-    appId: "1:838723952514:web:34b3eabaa49a73176db99b"
+  authDomain: "firestore-9ee8b.firebaseapp.com",
+  databaseURL: "https://firestore-9ee8b.firebaseio.com",
+  projectId: "firestore-9ee8b",
+  storageBucket: "firestore-9ee8b.appspot.com",
+  messagingSenderId: "838723952514",
+  appId: "1:838723952514:web:bce413dc516cb8da6db99b"
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,7 @@ export const environment = {
     projectId: "firestore-9ee8b",
     storageBucket: "firestore-9ee8b.appspot.com",
     messagingSenderId: "838723952514",
-    appId: "1:838723952514:web:34b3eabaa49a73176db99b"
+    appId: "1:838723952514:web:bce413dc516cb8da6db99b"
   }
 };
 
